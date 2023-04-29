c5f8e8a17f0bc75d4c492fadae2e079c7cc95118
Олег <mishin415@yandex.ru>
2023-04-29 21:41:25 +0300

    odd_doubled_numbers

---
+++ Homework/dz_3/task_1.py
@@ -0,0 +1,15 @@
+# Написать программу, которая принимает на вход список чисел и возвращает список,
+# содержащий только нечетные числа, умноженные на два.
+# При этом, использовать функции filter, map и лямбда-функцию.
+
+def get_odd_doubled_numbers(numbers: list) -> list:
+    return list(map(lambda x: x*2, filter(lambda x: x % 2 != 0, numbers)))
+def test_get_odd_doubled_numbers():
+    assert get_odd_doubled_numbers([1, 2, 3, 4, 5]) == [2, 6, 10]
+    assert get_odd_doubled_numbers([10, 20, 30]) == []
+    assert get_odd_doubled_numbers([]) == []
+    assert get_odd_doubled_numbers([1, 3, 5]) == [2, 6, 10]
+    assert get_odd_doubled_numbers([2, 4, 6]) == []
+    print("All test_get_odd_doubled_numbers passed!")
+
+test_get_odd_doubled_numbers()
