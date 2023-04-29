6c9c3293308ee293c70c2a98f112196c1c270ab8
Олег <mishin415@yandex.ru>
2023-04-29 21:53:58 +0300

    odd_doubled_numbers

--- Homework/dz_3/task_1.py
+++ Homework/dz_3/task_1.py
@@ -4,6 +4,10 @@
 
 def get_odd_doubled_numbers(numbers: list) -> list:
     return list(map(lambda x: x*2, filter(lambda x: x % 2 != 0, numbers)))
+#Эта функция принимает список чисел numbers и возвращает список,
+# содержащий только нечетные числа, умноженные на два.
+#Сначала с помощью filter() отбираются нечетные числа,
+# а затем с помощью map() они умножаются на два. Результат возвращается в виде списка.
 def test_get_odd_doubled_numbers():
     assert get_odd_doubled_numbers([1, 2, 3, 4, 5]) == [2, 6, 10]
     assert get_odd_doubled_numbers([10, 20, 30]) == []
